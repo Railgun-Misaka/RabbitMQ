a44679e60341d9f8481720fb67ca578d57914af3
Railgun-Misaka <54017650+Railgun-Misaka@users.noreply.github.com>
2020-03-16 21:58:09 +0800

    Update RabbitMQConsumer.java

--- src/main/java/com/rabbitmq/cp/RabbitMQConsumer.java
+++ src/main/java/com/rabbitmq/cp/RabbitMQConsumer.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
 
-@Component
 @RabbitListener(queues = "direct")
 public class RabbitMQConsumer {
 	
